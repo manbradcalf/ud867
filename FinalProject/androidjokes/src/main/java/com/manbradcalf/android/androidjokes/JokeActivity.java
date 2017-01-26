87d9a1029becafc314309d578aca55d97ff33be1
Ben Medcalf <ben.medcalf@gmail.com>
2017-01-26 12:54:44 -0500

    Completed step 2:
    I was an idiot and half awake last night and was trying to pull an intent extra out of a non existent savedInstanceState. Fixed that

--- FinalProject/androidjokes/src/main/java/com/manbradcalf/android/androidjokes/JokeActivity.java
+++ FinalProject/androidjokes/src/main/java/com/manbradcalf/android/androidjokes/JokeActivity.java
@@ -1,19 +1,23 @@
 package com.manbradcalf.android.androidjokes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
 
+    TextView jokeTextView;
     private final String JOKE_KEY = "joke";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView jokeTextView = (TextView) findViewById(R.id.joke_textview);
-        jokeTextView.setText(savedInstanceState.getString(JOKE_KEY));
+        setContentView(R.layout.activity_joke);
+        jokeTextView = (TextView) findViewById(R.id.joke_textview);
+        Intent intent = getIntent();
+        String joke = intent.getStringExtra(JOKE_KEY);
+        jokeTextView.setText(joke);
     }
 }
