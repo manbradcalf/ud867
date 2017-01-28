9c034194f9cc2367525890258240983356b54ada
Ben Medcalf <ben.medcalf@gmail.com>
2017-01-28 11:59:25 -0500

    completing step 2 and starting step 3 (whoops)

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,7 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncTask<Pair<Context, String>, Void, String> task = new EndpointsAsyncTask();
+        Pair pair = new Pair<Context, String>(this, "Test the toasty!");
+        task.execute(pair);
     }
 
 
---
+++ FinalProject/backend/src/main/java/com/manbradcalf/android/finalproject/backend/MyBean.java
@@ -0,0 +1,18 @@
+package com.manbradcalf.android.finalproject.backend;
+
+/** The object model for the data we are sending through endpoints */
+public class MyBean
+{
+
+    private String myData;
+
+    public String getData()
+    {
+        return myData;
+    }
+
+    public void setData(String data)
+    {
+        myData = data;
+    }
+}
