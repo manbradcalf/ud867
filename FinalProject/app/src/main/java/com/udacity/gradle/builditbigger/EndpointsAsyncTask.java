c77f3753165ddebb0302ce89b1a60b1fe0598b55
Ben Medcalf <ben.medcalf@gmail.com>
2017-01-29 16:27:57 -0500

    trying to set up the backend module to be used on a physical device. getting a 404. investigating now

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -3,6 +3,7 @@ package com.udacity.gradle.builditbigger;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v4.util.Pair;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
@@ -33,7 +34,7 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context,String >, Void, S
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl("http://10.0.0.12:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer()
                     {
                         @Override
@@ -63,6 +64,7 @@ public class EndpointsAsyncTask extends AsyncTask<Pair<Context,String >, Void, S
     @Override
     protected void onPostExecute(String result) {
         Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        Log.d(this.getClass().getSimpleName(), "onPostExecute called at least?");
     }
 }
 
